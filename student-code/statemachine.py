0005f38ae58043aca5309df2ae65bb3b37f40d97
Cayden R. Haun <floridaman7588@gmail.com>
2024-09-16 18:22:28 -0500

    Create statemachine.py

---
+++ student-code/statemachine.py
@@ -0,0 +1,7 @@
+# Hello! This repository is for SMNW 1982 Students to learn to code in python for the upcoming FRC season
+# This project is the design of a simple state machine, which is based on the flowchart in class.
+# Write your code below, and delete these comments if you want to.
+def main():
+    print("I'm not a state machine yet!")
+    
+main()
