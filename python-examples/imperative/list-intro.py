483c4bd43e93f73905e2404acc5c7e9f877e4003
Glitchy <glitchneonyt@gmail.com>
2024-09-16 19:09:23 -0500

    Calculator
    Teaches the students about functions and helps to explain lists.

---
+++ python-examples/imperative/list-intro.py
@@ -0,0 +1,44 @@
+# Imports
+import random
+
+# Create List(s)
+liste = []
+
+
+# Functions
+def add(num1, num2):
+    # Add the two variables
+    num3 = num1 + num2
+    num3 = str(num3)
+    print("Your added total is", num3)
+
+def subtract(num1, num2):
+    num3 = num2 - num1
+    num3 = str(num3)
+    print("Your subtracted total is", num3)
+
+
+# Main loop
+while 7 == 7:
+    # Generate the Values
+    num1 = input('input a random number:')
+    num2 = input('input a second random number:')
+
+    # Convert the Values
+    num1 = int(num1)
+    num2 = int(num2)
+
+    # Add them to the list
+    liste.append(num1)
+    liste.append(num2)
+    act = input("What to do now?")
+    if act == "add":
+        add(num1, num2)
+    elif act == "subtract":
+        subtract(num1, num2)
+    elif act == "list":
+        print(liste)
+    elif act == "clear":
+        liste = []
+    else:
+        print("Please try again.")
