8951970895b9425b004aacdb8d79cd6f99f959b9
Glitchy <glitchneonyt@gmail.com>
2024-09-18 07:29:06 -0500

    Update list-intro.py
    Because its fun @FloridaMan7588

--- python-examples/imperative/list-intro.py
+++ python-examples/imperative/list-intro.py
@@ -16,7 +16,7 @@ def subtract(num1, num2):
 
 
 # Main loop
-while True:
+while True:  # Same as while 7 == 7:
     # Generate the Values
     num1 = input('input a random number:')
     num2 = input('input a second random number:')
