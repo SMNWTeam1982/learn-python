ac8b548df23d4f7a17469f55a68c4ff53c8c58dc
Glitchy <glitchneonyt@gmail.com>
2024-09-26 10:37:11 -0500

    intro to classes
    unfinished, will do so later

---
+++ python-examples/imperative/introclass.py
@@ -0,0 +1,61 @@
+# start example
+class myclass:
+    x = 5
+
+
+p1 = myclass()
+print(p1.x)
+
+
+
+
+# The __init__() Function
+# The examples above are classes and objects in their simplest form, and are not really useful in real life applications.
+# To understand the meaning of classes we have to understand the built-in __init__() function.
+# All classes have a function called __init__(), which is always executed when the class is being initiated.
+# Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+
+
+# The __str__() Function
+# The __str__() function controls what should be returned when the class object is represented as a string.
+# If the __str__() function is not set, the string representation of the object is returned:
+
+#The string representation of an object WITHOUT the __str__() function:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1)
+
+#The string representation of an object WITH the __str__() function:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def __str__(self):
+    return f"{self.name}({self.age})"
+
+p1 = Person("John", 36)
+
+print(p1)
+
+
+#Object Methods
+# Objects can also contain methods. Methods in objects are functions that belong to the object.
+# Let us create a method in the Person class:
