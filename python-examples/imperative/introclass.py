d7d5f757887305aae4a03c9340ac0bbc9f6d90da
Glitchy <glitchneonyt@gmail.com>
2024-09-26 11:56:09 -0500

    Intro To Classes
    Finished it, will go over on Thursday 9/26 if Advent of Code from previous day is finished in time.

--- python-examples/imperative/introclass.py
+++ python-examples/imperative/introclass.py
@@ -59,3 +59,77 @@ print(p1)
 #Object Methods
 # Objects can also contain methods. Methods in objects are functions that belong to the object.
 # Let us create a method in the Person class:
+
+#Insert a function that prints a greeting, and execute it on the p1 object:
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
+
+
+
+
+# The self Parameter
+# The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
+# It does not have to be named self , you can call it whatever you like, but it has to be the first parameter of any function in the class:
+
+#Use the words mysillyobject and abc instead of self:
+
+class Person:
+  def __init__(mysillyobject, name, age):
+    mysillyobject.name = name
+    mysillyobject.age = age
+
+  def myfunc(abc):
+    print("Hello my name is " + abc.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
+
+
+
+# Modify Object Properties
+# You can modify properties on objects like this:
+
+#Set the age of p1 to 40:
+
+p1.age = 40
+
+
+#You can delete properties on objects by using the del keyword:
+
+# Delete the age property from the p1 object:
+
+del p1.age
+
+#You can delete objects by using the del keyword:
+
+#Delete the p1 object:
+
+del p1
+
+
+
+
+
+#The pass Statement
+#class definitions cannot be empty, but if you for some reason have a class definition with no content, put in the pass statement to avoid getting an error.
+
+class Person:
+  pass
+
+
+
+# link to study notes below, ask that students take the small quiz at the bottom
+# https://www.w3schools.com/python/python_classes.asp
